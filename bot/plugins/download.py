8df5fce4264ac846c7cb5cf927b9d44328070eff
nyeinchan17204 <92505220+nyeinchan17204@users.noreply.github.com>
2022-08-14 17:24:02 +0630

    Update download.py

--- bot/plugins/download.py
+++ bot/plugins/download.py
@@ -1,44 +1,80 @@
 import os
 from pyrogram import Client, filters
 from bot.helpers.sql_helper import gDriveDB, idsDB
-from bot.helpers.utils import CustomFilters, humanbytes, find_starttostr, find_betweentwostr
-from bot.helpers.gdrive_utils import GoogleDrive 
+from bot.helpers.utils import (
+    CustomFilters,
+    humanbytes,
+    find_starttostr,
+    find_betweentwostr,
+)
+from bot.helpers.gdrive_utils import GoogleDrive
 from bot import DOWNLOAD_DIRECTORY, LOGGER
 from bot.config import Messages, BotCommands
 from pyrogram.errors import FloodWait, RPCError
 from bot.helpers.sql_helper import idsDB
 
+ep_num = {}
 
-@Client.on_message(filters.private & filters.incoming & (filters.document | filters.audio | filters.video) & CustomFilters.auth_users)
 
+@Client.on_message(filters.text)
+async def text_msg(client, message):
+    message_ids = message.id + 1
+    message_texts = message.text
+    ep_num[message_ids] = message_texts
+    print(ep_num)
+
+
+@Client.on_message(
+    filters.private
+    & filters.incoming
+    & (filters.document | filters.audio | filters.video)
+    & CustomFilters.auth_users
+)
 def _telegram_file(client, message):
     user_id = message.from_user.id
-    rname = message.caption+')'
-    given_name = idsDB.search_pname(user_id)
-    print(given_name)
-    if given_name == 'hola':
-        gname = find_starttostr(rname,0,')') + ')' + '.mp4'
+    message_ids = message.id
+    name_caption = message.caption
+    movie_name = idsDB.search_pname(user_id)
+    print(movie_name)
+    if movie_name != "hola" and message_ids in ep_num:
+        final_name = movie_name + " အပိုင်း (" + str(ep_num[message_ids]) + ").mp4"
     else:
-        gname = given_name + ' အပိုင်း(' + find_betweentwostr(rname, '(', ')') + ').mp4'
-    
-    print(gname)
-
-    sent_message = message.reply_text('🕵️**.ဖိုင်လင့်ကိုစစ်ဆေးနေပါသည်...**'+gname , quote=True)
+        name_spliter = name_caption.splitlines()
+        print(len(name_spliter))
+        if len(name_spliter) > 3:
+            final_name = name_spliter[0] + name_spliter[1] + name_spliter[2] + ".mp4"
+        else:
+            final_name = name_spliter[0] + ".mp4"
+    sent_message = message.reply_text(
+        "🕵️**.ဖိုင်လင့်ကိုစစ်ဆေးနေပါသည်...**" + final_name, quote=True
+    )
     if message.document:
         file = message.document
     elif message.video:
         file = message.video
     elif message.audio:
         file = message.audio
-    sent_message.edit(Messages.DOWNLOAD_TG_FILE.format(file.file_name, humanbytes(file.file_size), file.mime_type))
-    LOGGER.info(f'Download:{user_id}: {file.file_id}')
+    sent_message.edit(
+        Messages.DOWNLOAD_TG_FILE.format(
+            file.file_name, humanbytes(file.file_size), file.mime_type
+        )
+    )
+    LOGGER.info(f"Download:{user_id}: {file.file_id}")
     try:
-        dl_name = os.path.join(f'{DOWNLOAD_DIRECTORY}/{gname}')
+        dl_name = os.path.join(f"{DOWNLOAD_DIRECTORY}/{final_name}")
         file_path = message.download(file_name=dl_name)
-        sent_message.edit(Messages.DOWNLOADED_SUCCESSFULLY.format(os.path.basename(file_path), humanbytes(os.path.getsize(file_path))))
+        sent_message.edit(
+            Messages.DOWNLOADED_SUCCESSFULLY.format(
+                os.path.basename(file_path), humanbytes(os.path.getsize(file_path))
+            )
+        )
         msg = GoogleDrive(user_id).upload_file(file_path, file.mime_type)
         sent_message.reply_text(msg)
     except RPCError:
         sent_message.edit(Messages.WENT_WRONG)
-    LOGGER.info(f'Deleteing: {file_path}')
+    LOGGER.info(f"Deleteing: {file_path}")
     os.remove(file_path)
+    if message_ids in ep_num:
+        ep_num.pop[message_ids]
+    else:
+        print("noting input")
